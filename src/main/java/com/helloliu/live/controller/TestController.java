bec21be2463e217d4528f30d0dc1ad73e52ad1c4
liuwei-pbj@hfax.com <liuwei-pbj@hfax.com>
2021-09-17 22:22:49 +0800

    集成mybatis

--- src/main/java/com/helloliu/live/controller/TestController.java
+++ src/main/java/com/helloliu/live/controller/TestController.java
@@ -1,13 +1,17 @@
 package com.helloliu.live.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
+    @Value("${test.hello:Test}")
+    private String testHello;
+
     @RequestMapping("/hello")
     public String hello(){
-        return "hello world";
+        return "hello world" + testHello;
     }
 }
